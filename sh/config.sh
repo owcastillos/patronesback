dd7ad1490822d7719cfd0fc482cee0788ade465b
Otto Castillo <owcastillos@unal.edu.co>
2022-11-14 23:04:22 -0500

    :wrench: AB#201 Replacing values

--- sh/config.sh
+++ sh/config.sh
@@ -1,5 +1,5 @@
-aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
-aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+aws configure set aws_access_key_id |AWS_ACCESS_KEY_ID|
+aws configure set aws_secret_access_key |AWS_SECRET_ACCESS_KEY|
 
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 596779343245.dkr.ecr.us-east-2.amazonaws.com/simcf-backend
 aws eks --region us-east-2 update-kubeconfig --name simcf-cluster
