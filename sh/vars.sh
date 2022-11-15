d79a39e99a51bd50510b67a045bade5a3aa9a826
Otto Castillo <owcastillos@unal.edu.co>
2022-11-14 23:28:06 -0500

    :wrench: AB#201 Fixing AWS settings

--- sh/vars.sh
+++ sh/vars.sh
@@ -3,3 +3,5 @@ sed 's/|AWS_SECRET_ACCESS_KEY|/'"$AWS_SECRET_ACCESS_KEY"'/' -i sh/config.sh
 
 sed 's/|AWS_ACCESS_KEY_ID|/'"$AWS_ACCESS_KEY_ID"'/' -i k8s-deploy.yml
 sed 's/|AWS_SECRET_ACCESS_KEY|/'"$AWS_SECRET_ACCESS_KEY"'/' -i k8s-deploy.yml
+
+cat sh/config.sh
