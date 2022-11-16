05b5c9a7bb4d4e39c523fbb4ec83aa935c3ded6e
Otto Castillo <owcastillos@unal.edu.co>
2022-11-15 23:59:05 -0500

    :wrench: AB#163 Adding delay time to get URL

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,5 +1,6 @@
 kubectl delete service/simcf-backend-service
 kubectl delete deployment.apps/simcf-backend-deploy
 kubectl create -f k8s-deploy.yml
+sleep 10
 kubectl get service simcf-backend-service
 echo 'Update URL service in front aplication'
