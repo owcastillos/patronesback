dfdfdfa5ea999932d1fce675bfa789139eacaffa
Otto Castillo <owcastillos@unal.edu.co>
2022-11-16 12:38:08 -0500

    :wrench: AB#163 Reverting some configuration

--- sh/vars.sh
+++ sh/vars.sh
@@ -1,2 +1,5 @@
 sed 's/|AWS_ACCESS_KEY_ID|/'"$AWS_ACCESS_KEY_ID"'/' -i sh/config.sh
 sed 's/|AWS_SECRET_ACCESS_KEY|/'"$AWS_SECRET_ACCESS_KEY"'/' -i sh/config.sh
+
+sed 's/|AWS_ACCESS_KEY_ID|/'"$AWS_ACCESS_KEY_ID"'/' -i k8s-deploy.yml
+sed 's/|AWS_SECRET_ACCESS_KEY|/'"$AWS_SECRET_ACCESS_KEY"'/' -i k8s-deploy.yml
